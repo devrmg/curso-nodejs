7b73a3ea53a0846c042f79d96bc88fa0e2a891c1
Renan Mendes <renanmendescontact@gmail.com>
2019-10-17 19:19:57 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+
+console.log("Hello World ");
+console.log("Meu nome é Renan")
+console.log("E eu estou aprendendo Node.js")
+
+if(mostrarSite){ // Se mostrar site
+  console.log(site); // Print o site
+}
