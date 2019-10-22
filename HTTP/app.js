9475dca56faa6604e4aed751c8e5434fdff72a32
Renan Mendes <renanmendescontact@gmail.com>
2019-10-22 20:33:55 -0300

    Update app.js

--- HTTP/app.js
+++ HTTP/app.js
@@ -1,7 +1,10 @@
 var http = require("http");
 
 http.createServer(function(requisicao,resposta){
-    resposta.end("<h1>Bem vindo ao meu site!</h1><h4>guiadoprogramador.com</h4>");
+    resposta.end("<h1>Olá, essa e minha primeira aplicação HTTP com Noje.js</h1><h4> https://devrmg.github.io/devrm/</h4>");
 }).listen(3000);
 
 console.log("Meu servidor está rodando!");
+
+var http = require("http");
+
